0c3ee6b57e7c024bb81e1d8ed80b3ce1a14644a7
ayush-volmex <ayusht11@outlook.com>
2021-08-18 10:35:43 +0530

    VOL-210: Added repricer constructor body

---
+++ contracts/repricers/Repricer.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: BUSL-1.1
+
+pragma solidity =0.7.6;
+
+import "@openzeppelin/contracts/math/SafeMath.sol";
+
+import "../oracles/IVolmexOracle.sol";
+
+contract Repricer {
+    using SafeMath for uint256;
+
+    event UpdatedLeverageCoefficient(
+        uint256 leverageCoefficient
+    );
+
+    uint256 public leverageCoefficient;
+
+    IVolmexOracle public oracle;
+}
