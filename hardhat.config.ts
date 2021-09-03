59731a448cd88e4f3387147dc0e74c5352e96e1a
ayush-volmex <ayusht11@outlook.com>
2021-09-03 12:51:24 +0530

    VOL-192: Added goerli and ganache networks

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,3 +1,4 @@
+import "@nomiclabs/hardhat-ganache";
 import "@typechain/hardhat";
 import "@nomiclabs/hardhat-waffle";
 import "@nomiclabs/hardhat-web3";
@@ -5,6 +6,7 @@ import "@nomiclabs/hardhat-ethers";
 import "@nomiclabs/hardhat-etherscan";
 import "@nomiclabs/hardhat-truffle5";
 import "@nomiclabs/hardhat-ganache";
+import "@openzeppelin/hardhat-upgrades";
 
 import "hardhat-gas-reporter";
 import "hardhat-deploy";
@@ -45,6 +47,12 @@ const config: HardhatUserConfig = {
     hardhat: {
       allowUnlimitedContractSize: false,
     },
+    localhost: {
+      url: "http://127.0.0.1:8545", // same address and port for both Buidler and Ganache node
+      accounts: ['0xfd70339075a2b35b739074f5fd21464dea33faa47f3b3884724139dcdaad58fe'],
+      gas: 8000000,
+      gasPrice: 1,
+    },
     rinkeby: {
       url: `https://eth-rinkeby.alchemyapi.io/v2/${process.env.RINKEBY_ALCHEMY_API_KEY}`,
       accounts: [`0x${process.env.PRIVATE_KEY}`],
@@ -52,6 +60,16 @@ const config: HardhatUserConfig = {
       loggingEnabled: true,
       gas: 5000000,
       gasPrice: 10000000000
+    },
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${process.env.GOERLI_ALCHEMY_API_KEY}`,
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+      throwOnTransactionFailures: true,
+      loggingEnabled: true,
+      gas: 5000000,
+      gasPrice: 200000000000,
+      blockGasLimit: 8000000,
+      timeout: 10800000
     }
   },
   etherscan: {
