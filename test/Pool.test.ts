054b2ec2c410633269c74e5925a5b61ce2a44389
ayush-volmex <ayusht11@outlook.com>
2021-09-19 17:19:13 +0530

    VOL-226: Added tests for expect revert of pool contract

--- test/Pool.test.ts
+++ test/Pool.test.ts
@@ -90,6 +90,27 @@ describe('Pool', function () {
     const leverageComplement = '1000003521850180000';
     const MAX = '10000000000000000000000';
 
+    await (await collateral.mint(owner, MAX)).wait();
+    await (await collateral.approve(protocol.address, MAX)).wait();
+    await (await protocol.collateralize(MAX)).wait();
+    await (await volatility.approve(pool.address, MAX)).wait();
+    await (await inverseVolatility.approve(pool.address, MAX)).wait();
+
+    // Test non-controller finalise
+    await expectRevert(
+      pool.finalize(
+        '1000000000000000000',
+        leveragePrimary,
+        '1000000000000000000',
+        leverageComplement,
+        exposureLimitPrimary,
+        exposureLimitComplement,
+        pMin,
+        qMin
+      ),
+      'NOT_SET_FEE_PARAMS'
+    );
+
     const feereceipt = await pool.setFeeParams(baseFee, maxFee, feeAmpPrimary, feeAmpComplement);
     await feereceipt.wait();
 
@@ -108,11 +129,65 @@ describe('Pool', function () {
       'NOT_FINALIZED'
     );
 
-    await (await collateral.mint(owner, '10000000000000000000000')).wait();
-    await (await collateral.approve(protocol.address, MAX)).wait();
-    await (await protocol.collateralize('4000000000000000000000')).wait();
-    await (await volatility.approve(pool.address, MAX)).wait();
-    await (await inverseVolatility.approve(pool.address, MAX)).wait();
+    // Test non-controller finalise
+    await expectRevert(
+      pool.connect(accounts[1]).finalize(
+        '1000000000000000000',
+        leveragePrimary,
+        '1000000000000000000',
+        leverageComplement,
+        exposureLimitPrimary,
+        exposureLimitComplement,
+        pMin,
+        qMin
+      ),
+      'NOT_CONTROLLER'
+    );
+
+    // Test tokens balance
+    await expectRevert(
+      pool.finalize(
+        '1000000000000000000',
+        leveragePrimary,
+        '100000000000000000',
+        leverageComplement,
+        exposureLimitPrimary,
+        exposureLimitComplement,
+        pMin,
+        qMin
+      ),
+      'NOT_SYMMETRIC'
+    );
+
+    // Test the bind require checks
+    await expectRevert(
+      pool.finalize(
+        '100000',
+        leveragePrimary,
+        '100000',
+        leverageComplement,
+        exposureLimitPrimary,
+        exposureLimitComplement,
+        pMin,
+        qMin
+      ),
+      'MIN_BALANCE'
+    );
+
+    // Test token leverage
+    await expectRevert(
+      pool.finalize(
+        '1000000000000000000',
+        '0',
+        '1000000000000000000',
+        '0',
+        exposureLimitPrimary,
+        exposureLimitComplement,
+        pMin,
+        qMin
+      ),
+      'ZERO_LEVERAGE'
+    );
 
     const finalizeReceipt = await pool.finalize(
       '1000000000000000000',
@@ -149,18 +224,18 @@ describe('Pool', function () {
     expect(swapreceipt.confirmations).equal(1);
   });
 
-  it('Should user exit liquidity from pool', async () => {
+  it('Should user eit liquidity from pool', async () => {
     const joinReceipt = await pool.joinPool(
       '3000000000000000000000',
       ['20000000000000000000','20000000000000000000']
     );
     await joinReceipt.wait();
 
-    const exitReceipt = await pool.exitPool(
+    const eitReceipt = await pool.exitPool(
       '1000000000000000000000',
       ['1000000000000000000','1000000000000000000']
     );
-    await exitReceipt.wait();
+    await eitReceipt.wait();
   });
 
   it('Should volatility tokens', async () => {
@@ -235,7 +310,7 @@ describe('Pool', function () {
     );
   });
 
-  it('Should revert set fee when finalzed', async () => {
+  it('Should revert set fee when finalized', async () => {
     const baseFee = (0.02 * Math.pow(10, 18)).toString();
     const maxFee = (0.4 * Math.pow(10, 18)).toString();
     const feeAmpPrimary = 10;
@@ -244,6 +319,133 @@ describe('Pool', function () {
     await expectRevert(
       pool.setFeeParams(baseFee, maxFee, feeAmpPrimary, feeAmpComplement),
       'IS_FINALIZED'
+    );
+  });
+
+  it('Should revert when finalize call', async () => {
+    const qMin = (1 * Math.pow(10, 6)).toString();
+    const pMin = (0.01 * Math.pow(10, 18)).toString();
+    const exposureLimitPrimary = (0.25 * Math.pow(10, 18)).toString();
+    const exposureLimitComplement = (0.25 * Math.pow(10, 18)).toString();
+    const leveragePrimary = '999996478162223000';
+    const leverageComplement = '1000003521850180000';
+
+    await expectRevert(
+      pool.finalize(
+        '1000000000000000000',
+        leveragePrimary,
+        '1000000000000000000',
+        leverageComplement,
+        exposureLimitPrimary,
+        exposureLimitComplement,
+        pMin,
+        qMin
+      ),
+      'IS_FINALIZED'
+    );
+  });
+
+  it('Should revert join pool', async () => {
+    await expectRevert(
+      pool.joinPool(
+        '0',
+        ['20000000000000000000','20000000000000000000']
+      ),
+      'MATH_APPROX'
+    );
+
+    await expectRevert(
+      pool.joinPool(
+        '3000000000000000000000',
+        ['2000000000000000000','2000000000000000000']
+      ),
+      'LIMIT_IN'
+    );
+  });
+
+  it('Should revert eit pool', async () => {
+    await expectRevert(
+      pool.exitPool(
+        '0',
+        ['1000000000000000000','1000000000000000000']
+      ),
+      'MATH_APPROX'
+    );
+
+    await expectRevert(
+      pool.exitPool(
+        '250000000000000000000',
+        ['2000000000000000000','2000000000000000000']
+      ),
+      'LIMIT_OUT'
+    );
+  });
+
+  it('Should revert swap', async () => {
+    await expectRevert(
+      pool.swapExactAmountIn(
+        volatility.address,
+        '3000000000000000000',
+        volatility.address,
+        '1000000000000000000'
+      ),
+      'SAME_TOKEN'
+    );
+
+    await expectRevert(
+      pool.swapExactAmountIn(
+        volatility.address,
+        '100000',
+        inverseVolatility.address,
+        '1000000000000000000'
+      ),
+      'MIN_TOKEN_IN'
+    );
+
+    await expectRevert(
+      pool.swapExactAmountIn(
+        volatility.address,
+        '6599999999999998746',
+        inverseVolatility.address,
+        '1000000000000000000'
+      ),
+      'MAX_IN_RATIO'
+    );
+  });
+
+  it('Should revert on limit out', async () => {
+    const joinReceipt = await pool.joinPool(
+      '3000000000000000000000',
+      ['20000000000000000000','20000000000000000000']
+    );
+    await joinReceipt.wait();
+
+    await expectRevert(
+      pool.swapExactAmountIn(
+        volatility.address,
+        '3000000000000000000',
+        inverseVolatility.address,
+        '3000000000000000000'
+      ),
+      'LIMIT_OUT'
+    );
+  });
+
+  it('Should revert require boundary exposure', async () => {
+    const joinReceipt = await pool.joinPool(
+      '4000000000000000000000',
+      ['16000000000000000000','16000000000000000000']
+    );
+    await joinReceipt.wait();
+
+    await expectRevert(
+      pool.swapExactAmountIn(
+        volatility.address,
+        '8000000000000000000',
+        inverseVolatility.address,
+        '1000000000000000000'
+      ),
+      'BOUNDARY_EXPOSURE'
     );
   });
 });
