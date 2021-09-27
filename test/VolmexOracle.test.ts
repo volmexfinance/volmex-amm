8f34fa28a4e349d632a75d3dbb933987b7b8d8b5
ayush-volmex <ayusht11@outlook.com>
2021-09-27 15:06:56 +0530

    VOL-242: Updated the unit tests of oracle

--- test/VolmexOracle.test.ts
+++ test/VolmexOracle.test.ts
@@ -1,10 +1,9 @@
-const { expect } = require("chai");
-const { ethers, upgrades } = require("hardhat");
-import { Signer } from "ethers";
-const { expectRevert } = require("@openzeppelin/test-helpers");
+const { expect } = require('chai');
+const { ethers, upgrades } = require('hardhat');
+import { Signer } from 'ethers';
+const { expectRevert } = require('@openzeppelin/test-helpers');
 
-
-describe("Volmex Oracle", function () {
+describe('Volmex Oracle', function () {
   let accounts: Signer[];
   let volmexOracleFactory: any;
   let volmexOracle: any;
@@ -12,9 +11,7 @@ describe("Volmex Oracle", function () {
   this.beforeAll(async function () {
     accounts = await ethers.getSigners();
 
-    volmexOracleFactory = (await ethers.getContractFactory(
-      "VolmexOracle"
-    ));
+    volmexOracleFactory = await ethers.getContractFactory('VolmexOracle');
   });
 
   this.beforeEach(async function () {
@@ -23,37 +20,40 @@ describe("Volmex Oracle", function () {
     await volmexOracle.deployed();
   });
 
-  it ("Should deplloy volmex oracle", async () => {
+  it('Should deploy volmex oracle', async () => {
     const receipt = await volmexOracle.deployed();
     expect(receipt.confirmations).not.equal(0);
   });
 
-  it ("Should update the volatility price", async () => {
-    const receipt = await volmexOracle.updateVolatilityTokenPrice(
-      "0",
-      "105"
-    );
+  it('Should update the volatility price', async () => {
+    const receipt = await volmexOracle.updateVolatilityTokenPrice('0', '105');
 
     expect((await receipt.wait()).confirmations).not.equal(0);
 
-    expect(await volmexOracle.volatilityTokenPriceByIndex("0")).equal("1050000");
+    expect(await volmexOracle.volatilityTokenPriceByIndex('0')).equal('1050000');
   });
 
-  it ("Should revert when volatility price is not in range", async () => {
+  it('Should revert when volatility price is not in range', async () => {
     await expectRevert(
-      volmexOracle.updateVolatilityTokenPrice(
-        "0",
-        "250"
-      ),
-      "VolmexOracle: _volatilityTokenPrice should be greater than 0"
+      volmexOracle.updateVolatilityTokenPrice('0', '250'),
+      'VolmexOracle: _volatilityTokenPrice should be greater than 0'
     );
 
     await expectRevert(
-      volmexOracle.updateVolatilityTokenPrice(
-        "1",
-        "0"
-      ),
-      "VolmexOracle: _volatilityTokenPrice should be greater than 0"
+      volmexOracle.updateVolatilityTokenPrice('1', '0'),
+      'VolmexOracle: _volatilityTokenPrice should be greater than 0'
     );
+  });
+
+  it('Should add volatility token price', async () => {
+    await expectRevert(
+      volmexOracle.addVolatilityTokenPrice('0', 'LTCV'),
+      'VolmexOracle: _volatilityTokenPrice should be greater than 0'
+    );
+
+    const receipt = await volmexOracle.addVolatilityTokenPrice('120', 'LTCV');
+    expect((await receipt.wait()).confirmations).not.equal(0);
+
+    expect(await volmexOracle.volatilityTokenPriceByIndex(await volmexOracle.indexCount())).equal('1200000');
   });
 });
