fba6d415ccc884a8848ca85d0ca766e86e7f28ec
ayush-volmex <ayusht11@outlook.com>
2021-10-05 17:12:32 +0530

    VOL-270: Moved unused files to deprecated

---
+++ contracts/deprecated/PoolBuilder.sol
@@ -0,0 +1,29 @@
+// "SPDX-License-Identifier: GNU General Public License v3.0"
+
+pragma solidity 0.7.6;
+
+// import './Pool.sol';
+// import './IPoolBuilder.sol';
+
+// contract PoolBuilder is IPoolBuilder {
+//     function buildPool(
+//         address _controller,
+//         address _derivativeVault,
+//         address _feeCalculator,
+//         address _repricer,
+//         //TODO: remove params when a new factory is deploying
+//         uint256 _baseFee,
+//         uint256 _maxFee,
+//         uint256 _feeAmp
+//     ) public override returns (address) {
+//         Pool pool =
+//             new Pool(
+//                 _derivativeVault,
+//                 _feeCalculator,
+//                 _repricer,
+//                 _controller
+//             );
+//         pool.transferOwnership(msg.sender);
+//         return address(pool);
+//     }
+// }
