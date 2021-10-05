d86f363e417753b6719cf4059f1a7026c2cf55cd
ayush-volmex <ayusht11@outlook.com>
2021-10-05 17:03:38 +0530

    VOL-260: Added comments and removed aadmin fee

--- contracts/Controller.sol
+++ contracts/Controller.sol
@@ -8,20 +8,35 @@ import './IPool.sol';
 import './interfaces/IVolmexProtocol.sol';
 import './interfaces/IERC20Modified.sol';
 
+/**
+ * @title Volmex Controller contract
+ * @author volmex.finance [security@volmexlabs.com]
+ */
 contract Controller is OwnableUpgradeable {
     event AdminFeeUpdated(uint256 adminFee);
 
     event AssetSwaped(uint256 assetInAmount, uint256 assetOutAmount);
 
+    // Address of the collateral used in protocol
     IERC20Modified public stablecoin;
+    // Address on the pool contract
     IPool public pool;
+    // Address of the protocol contract
     IVolmexProtocol public protocol;
+    // Ratio of volatility to be minted per 250 collateral
     uint256 private _volatilityCapRatio;
+    // Minimum amount of collateral amount needed to collateralize
     uint256 private _minimumCollateralQty;
 
-    uint256 public adminFee;
-    uint256 public constant MAX_FEE = 100;
-
+    /**
+     * @notice Initializes the contract
+     *
+     * @dev Sets the volatilityCapRatio and _minimumCollateralQty
+     *
+     * @param _stablecoin Address of the collateral token used in protocol
+     * @param _pool Address of the pool contract
+     * @param _protocol Address of the protocol contract
+     */
     function initialize(
         IERC20Modified _stablecoin,
         IPool _pool,
@@ -33,17 +48,19 @@ contract Controller is OwnableUpgradeable {
 
         _volatilityCapRatio = protocol.volatilityCapRatio();
         _minimumCollateralQty = protocol.minimumCollateralQty();
-
-        adminFee = 10;
     }
 
-    function updateAdminFee(uint256 _adminFee) external onlyOwner {
-        require(_adminFee <= MAX_FEE, 'Controller: _adminFee OUT_OF_RANGE');
-        adminFee = _adminFee;
-
-        emit AdminFeeUpdated(_adminFee);
-    }
-
+    /**
+     * @notice Used to swap collateral token to a type of volatility token
+     *
+     * @dev Amount if transferred to the controller and approved for pool
+     * @dev collateralize the amount to get volatility
+     * @dev Swaps half the quantity of volatility asset using pool contract
+     * @dev Transfers the asset to caller
+     *
+     * @param _amount Amount of collateral token
+     * @param _isInverseRequired Bool value token type required { true: iETHV, false: ETHV }
+     */
     function swapCollateralToVolatility(uint256 _amount, bool _isInverseRequired) external {
         stablecoin.transferFrom(msg.sender, address(this), _amount);
         stablecoin.approve(address(protocol), _amount);
@@ -82,6 +99,17 @@ contract Controller is OwnableUpgradeable {
         emit AssetSwaped(_amount, volatilityAmount + tokenAmountOut);
     }
 
+    /**
+     * @notice Used to swap a type of volatility token to collateral token
+     *
+     * @dev Amount if transferred to the controller and approved for pool
+     * @dev Swaps half the quantity of volatility asset using pool contract
+     * @dev redeems the amount to get collateral
+     * @dev Transfers the asset to caller
+     *
+     * @param _amount Amount of volatility token
+     * @param _isInverseRequired Bool value token type required { true: iETHV, false: ETHV }
+     */
     function swapVolatilityToCollateral(uint256 _amount, bool _isInverseRequired) external {
         IERC20Modified volatilityToken = protocol.volatilityToken();
         IERC20Modified inverseVolatilityToken = protocol.inverseVolatilityToken();
