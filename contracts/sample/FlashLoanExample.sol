d699da3e69ab7358f6ebeeb85a2f69107f62c1f9
ayush-volmex <ayusht11@outlook.com>
2021-10-27 15:35:10 +0530

    VOL-310: Added flash loan example contract

---
+++ contracts/sample/FlashLoanExample.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: BUSL-1.1
+
+pragma solidity =0.7.6;
+
+import '../interfaces/IFlashLoanReceiver.sol';
+import '../Num.sol';
+
+contract FlashLoanExample is Num {
+    /**
+        This function is called after your contract has received the flash loaned amount
+     */
+    function executeOperation(
+        address asset,
+        uint256 amount,
+        uint256 premium,
+        address initiator,
+        bytes calldata params
+    ) external returns (bool) {
+        //
+        // This contract now has the funds requested.
+        // Your logic goes here.
+        //
+
+        // At the end of your logic above, this contract owes
+        // the flashloaned amounts + premiums.
+        // Therefore ensure your contract has enough to repay
+        // these amounts.
+
+        // Approve the Pool contract allowance to *pull* the owed amount
+        uint256 amountOwing = add(amount, premium);
+        IERC20Modified(asset).approve(address(IFlashLoanReceiver(initiator).POOL()), amountOwing);
+
+        return true;
+    }
+}
