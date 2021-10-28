b1e8aa7080af97845a90ddac858df8a787cf4207
ayush-volmex <ayusht11@outlook.com>
2021-10-28 10:33:08 +0530

    VOL-270: Updated script codes

--- scripts/verifyContract.ts
+++ scripts/verifyContract.ts
@@ -1,17 +1,16 @@
 import { run } from "hardhat";
 
 const verify = async () => {
-  const implementation = `0x11154fc9de55DBCf8670bC7c72cBf78781F3Df08`;
+  const implementation = process.env.IMPLEMENTATION;
 
   console.log("Verifying ", implementation);
 
   await run("verify:verify", {
     address: implementation,
     constructorArguments: [
-      '0x0d2497c1eCB40F77BFcdD99f04AC049c9E9d83F7',
-      '0x105aE5e940f157D93187082CafCCB27e1941B505',
-      '0x58b744ff6Ed0A47925b431c58d842817C9e82DB4',
-      '0x74bC67ed6948f0a4C387C353975F142Dc640537a'
+      /**
+       * Place constructor arguments here
+       */
     ]
   });
 };
