8957b45931e28621102584c2eb09b2130b99fe09
ayush-volmex <ayusht11@outlook.com>
2021-11-03 16:10:43 +0530

    Refactored AMM for flash loans

--- contracts/VolmexAMM.sol
+++ contracts/VolmexAMM.sol
@@ -60,7 +60,6 @@ contract VolmexAMM is OwnableUpgradeable, PausableUpgradeable, Token, Math, Toke
 
     event FlashLoan(
         address indexed target,
-        address indexed initiator,
         address indexed asset,
         uint256 amount,
         uint256 premium
@@ -74,8 +73,8 @@ contract VolmexAMM is OwnableUpgradeable, PausableUpgradeable, Token, Math, Toke
     // Used to prevent the re-entry
     bool private _mutex;
 
-    // Address of the pool deployer
-    address private deployer; // has CONTROL role
+    // Address of the pool controller
+    address private controller; // has CONTROL role
 
     // `finalize` sets `PUBLIC can SWAP`, `PUBLIC can JOIN`
     bool private _finalized;
@@ -173,22 +172,30 @@ contract VolmexAMM is OwnableUpgradeable, PausableUpgradeable, Token, Math, Toke
     }
 
     /**
+     * @notice Used to check the caller is controller
+     */
+    modifier onlyController() {
+        require(msg.sender == controller, 'VolmexAMM: Caller is not controller');
+        _;
+    }
+
+    /**
      * @notice Initialize the pool contract with required elements
      *
      * @dev Checks, the protocol is a contract
-     * @dev Sets repricer, protocol and deployer addresses
+     * @dev Sets repricer, protocol and controller addresses
      * @dev Sets upperBoundary, volatilityIndex and denomination
      * @dev Make the AMM token name and symbol
      *
      * @param _repricer Address of the volmex repricer contract
      * @param _protocol Address of the volmex protocol contract
-     * @param _deployer Address of the pool contract deployer
+     * @param _controller Address of the pool contract controller
      * @param _volatilityIndex Index of the volatility price in oracle
      */
     function initialize(
         IVolmexRepricer _repricer,
         IVolmexProtocol _protocol,
-        address _deployer,
+        address _controller,
         uint256 _volatilityIndex
     ) external initializer {
         require(
@@ -197,8 +204,8 @@ contract VolmexAMM is OwnableUpgradeable, PausableUpgradeable, Token, Math, Toke
         );
         repricer = _repricer;
 
-        require(_deployer != address(0), 'VolmexAMM: Deployer can not be zero address');
-        deployer = _deployer;
+        require(_controller != address(0), 'VolmexAMM: Deployer can not be zero address');
+        controller = _controller;
 
         // NOTE: Intentionally skipped require check for protocol
         protocol = _protocol;
@@ -224,7 +231,7 @@ contract VolmexAMM is OwnableUpgradeable, PausableUpgradeable, Token, Math, Toke
     /**
      * @notice Sets all type of fees
      *
-     * @dev Checks the contract is finalised and caller is deployer of the pool
+     * @dev Checks the contract is finalised and caller is controller of the pool
      *
      * @param _baseFee Fee of the pool contract
      * @param _maxFee Max fee of the pool while swap
@@ -238,7 +245,7 @@ contract VolmexAMM is OwnableUpgradeable, PausableUpgradeable, Token, Math, Toke
         uint256 _feeAmpComplement
     ) external _logs_ _lock_ onlyNotSettled {
         require(!_finalized, 'VolmexAMM: AMM is finalized');
-        require(msg.sender == deployer, 'VolmexAMM: Caller is not a deployer');
+        require(msg.sender == controller, 'VolmexAMM: Caller is not a controller');
 
         baseFee = _baseFee;
         maxFee = _maxFee;
@@ -251,10 +258,10 @@ contract VolmexAMM is OwnableUpgradeable, PausableUpgradeable, Token, Math, Toke
     /**
      * @notice Used to finalise the pool with the required attributes and operations
      *
-     * @dev Checks, pool is finalised, caller is deployer, supplied token balance
+     * @dev Checks, pool is finalised, caller is controller, supplied token balance
      * should be equal
      * @dev Binds the token, and its leverage and balance
-     * @dev Calculates the iniyial pool supply, mints and transfer to the deployer
+     * @dev Calculates the iniyial pool supply, mints and transfer to the controller
      *
      * @param _primaryBalance Balance amount of primary token
      * @param _primaryLeverage Leverage value of primary token
@@ -276,7 +283,7 @@ contract VolmexAMM is OwnableUpgradeable, PausableUpgradeable, Token, Math, Toke
         uint256 _qMin
     ) external _logs_ _lock_ onlyNotSettled {
         require(!_finalized, 'VolmexAMM: AMM is finalized');
-        require(msg.sender == deployer, 'VolmexAMM: Caller is not a deployer');
+        require(msg.sender == controller, 'VolmexAMM: Caller is not a controller');
 
         require(_primaryBalance == _complementBalance, 'VolmexAMM: Assets balance should be same');
 
@@ -336,7 +343,7 @@ contract VolmexAMM is OwnableUpgradeable, PausableUpgradeable, Token, Math, Toke
         uint256 premium = div(mul(amount, FLASHLOAN_PREMIUM_TOTAL), 10000);
 
         require(
-            receiver.executeOperation(assetToken, amount, premium, msg.sender, params),
+            receiver.executeOperation(assetToken, amount, premium, receiverAddress, params),
             'VolmexAMM: Invalid flash loan executor'
         );
 
@@ -348,7 +355,6 @@ contract VolmexAMM is OwnableUpgradeable, PausableUpgradeable, Token, Math, Toke
 
         emit FlashLoan(
             receiverAddress,
-            msg.sender,
             assetToken,
             amount,
             premium
@@ -363,11 +369,16 @@ contract VolmexAMM is OwnableUpgradeable, PausableUpgradeable, Token, Math, Toke
      * @param poolAmountOut Amount of pool token mint and transfer to LP
      * @param maxAmountsIn Max amount of pool assets an LP can supply
      */
-    function joinPool(uint256 poolAmountOut, uint256[2] calldata maxAmountsIn)
+    function joinPool(
+        uint256 poolAmountOut,
+        uint256[2] calldata maxAmountsIn,
+        address receiver
+    )
         external
         _logs_
         _lock_
         onlyFinalized
+        onlyController
     {
         uint256 poolTotal = totalSupply();
         uint256 ratio = div(poolAmountOut, poolTotal);
@@ -383,12 +394,12 @@ contract VolmexAMM is OwnableUpgradeable, PausableUpgradeable, Token, Math, Toke
             uint256 tokenAmountIn = mul(ratio, bal);
             require(tokenAmountIn <= maxAmountsIn[i], 'VolmexAMM: Amount in limit exploit');
             _records[token].balance = add(_records[token].balance, tokenAmountIn);
-            emit LOG_JOIN(msg.sender, token, tokenAmountIn);
-            _pullUnderlying(token, msg.sender, tokenAmountIn);
+            emit LOG_JOIN(receiver, token, tokenAmountIn);
+            _pullUnderlying(token, receiver, tokenAmountIn);
         }
 
         _mintPoolShare(poolAmountOut);
-        _pushPoolShare(msg.sender, poolAmountOut);
+        _pushPoolShare(receiver, poolAmountOut);
     }
 
     /**
@@ -400,11 +411,16 @@ contract VolmexAMM is OwnableUpgradeable, PausableUpgradeable, Token, Math, Toke
      * @param poolAmountIn Amount of pool token transfer to the pool
      * @param minAmountsOut Min amount of pool assets an LP wish to redeem
      */
-    function exitPool(uint256 poolAmountIn, uint256[2] calldata minAmountsOut)
+    function exitPool(
+        uint256 poolAmountIn,
+        uint256[2] calldata minAmountsOut,
+        address receiver
+    )
         external
         _logs_
         _lock_
         onlyFinalized
+        onlyController
     {
         uint256 poolTotal = totalSupply();
         uint256 ratio = div(poolAmountIn, poolTotal);
@@ -417,11 +433,11 @@ contract VolmexAMM is OwnableUpgradeable, PausableUpgradeable, Token, Math, Toke
             uint256 tokenAmountOut = calculateAmountOut(poolAmountIn, ratio, bal);
             require(tokenAmountOut >= minAmountsOut[i], 'LIMIT_OUT');
             _records[token].balance = sub(_records[token].balance, tokenAmountOut);
-            emit LOG_EXIT(msg.sender, token, tokenAmountOut);
-            _pushUnderlying(token, msg.sender, tokenAmountOut);
+            emit LOG_EXIT(receiver, token, tokenAmountOut);
+            _pushUnderlying(token, receiver, tokenAmountOut);
         }
 
-        _pullPoolShare(msg.sender, poolAmountIn);
+        _pullPoolShare(receiver, poolAmountIn);
         _burnPoolShare(poolAmountIn);
     }
 
@@ -451,6 +467,7 @@ contract VolmexAMM is OwnableUpgradeable, PausableUpgradeable, Token, Math, Toke
         whenNotPaused
         onlyFinalized
         onlyNotSettled
+        onlyController
         returns (uint256 tokenAmountOut, uint256 spotPriceAfter)
     {
         require(tokenIn != tokenOut, 'VolmexAMM: Passed same token addresses');
@@ -651,78 +668,6 @@ contract VolmexAMM is OwnableUpgradeable, PausableUpgradeable, Token, Math, Toke
         _pushUnderlying(tokenOut, msg.sender, tokenAmountOut);
     }
 
-//    // Method temporary is not available for external usage.
-//    function swapExactAmountOut(
-//        address tokenIn,
-//        uint256 maxAmountIn,
-//        address tokenOut,
-//        uint256 tokenAmountOut
-//    )
-//        private
-//        _logs_
-//        _lock_
-//        whenNotPaused
-//        onlyFinalized
-//        onlyLiveDerivative
-//        returns (uint256 tokenAmountIn, uint256 spotPriceAfter)
-//    {
-//        require(tokenIn != tokenOut, 'SAME_TOKEN');
-//        require(tokenAmountOut >= qMin, 'MIN_TOKEN_OUT');
-//
-//        _reprice();
-//
-//        Record memory inRecord = _records[tokenIn];
-//        Record memory outRecord = _records[tokenOut];
-//
-//        require(
-//            tokenAmountOut <=
-//                mul(min(_getLeveragedBalance(outRecord), outRecord.balance), MAX_OUT_RATIO),
-//            'MAX_OUT_RATIO'
-//        );
-//
-//        tokenAmountIn = calcInGivenOut(
-//            _getLeveragedBalance(inRecord),
-//            _getLeveragedBalance(outRecord),
-//            tokenAmountOut,
-//            0
-//        );
-//
-//        uint256 fee;
-//        int256 expStart;
-//        (fee, expStart) = calcFee(
-//            inRecord,
-//            tokenAmountIn,
-//            outRecord,
-//            tokenAmountOut,
-//            _getPrimaryDerivativeAddress() == tokenIn ? feeAmpPrimary : feeAmpComplement
-//        );
-//
-//        uint256 spotPriceBefore =
-//            calcSpotPrice(
-//                _getLeveragedBalance(inRecord),
-//                _getLeveragedBalance(outRecord),
-//                0
-//            );
-//
-//        tokenAmountIn = calcInGivenOut(
-//            _getLeveragedBalance(inRecord),
-//            _getLeveragedBalance(outRecord),
-//            tokenAmountOut,
-//            fee
-//        );
-//
-//        require(tokenAmountIn <= maxAmountIn, 'LIMIT_IN');
-//
-//        spotPriceAfter = _performSwap(
-//            tokenIn,
-//            tokenAmountIn,
-//            tokenOut,
-//            tokenAmountOut,
-//            spotPriceBefore,
-//            fee
-//        );
-//    }
-
     function _getLeveragedBalance(Record memory r) internal pure returns (uint256) {
         return mul(r.balance, r.leverage);
     }
@@ -858,7 +803,7 @@ contract VolmexAMM is OwnableUpgradeable, PausableUpgradeable, Token, Math, Toke
     /**
      * @notice Used to bind the token, and its leverage and balance
      *
-     * @dev This method will transfer the provided assets balance to pool from deployer
+     * @dev This method will transfer the provided assets balance to pool from controller
      */
     function _bind(
         uint256 index,
