c0cf8e34980355b765c3c89919273e441f6b9044
ayush-volmex <ayusht11@outlook.com>
2021-11-03 17:40:48 +0530

    VOL-317: Added spdx license

--- contracts/libs/tokens/Token.sol
+++ contracts/libs/tokens/Token.sol
@@ -10,6 +10,7 @@
 
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// SPDX-License-Identifier: BUSL-1.1
 
 pragma solidity 0.7.6;
 
