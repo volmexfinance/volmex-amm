35430eb5996e28f6371a96a5b0632609df334134
ayush-volmex <ayusht11@outlook.com>
2021-12-13 12:31:18 +0530

    VOL-370: Updated controller interface

--- contracts/interfaces/IVolmexController.sol
+++ contracts/interfaces/IVolmexController.sol
@@ -8,7 +8,6 @@ interface IVolmexController {
     function transferAssetToPool(
         IERC20Modified _token,
         address _account,
-        address _pool,
         uint256 _amount
     ) external;
 }
