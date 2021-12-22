619f93097c4cb80c11097b93bce9c4cd4da5f1d7
ayush-volmex <ayush@volmexlabs.com>
2021-12-22 22:31:58 +0530

    VOL-509: removed unused function from inyerface

--- contracts/interfaces/IVolmexRepricer.sol
+++ contracts/interfaces/IVolmexRepricer.sol
@@ -5,7 +5,6 @@ pragma solidity =0.8.11;
 import '@openzeppelin/contracts-upgradeable/utils/introspection/IERC165Upgradeable.sol';
 
 interface IVolmexRepricer is IERC165Upgradeable {
-    function protocolVolatilityCapRatio() external view returns (uint256);
 
     function reprice(uint256 _volatilityIndex)
         external
