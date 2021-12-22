79462d17391e1a6eb13fa0a973045881fc7dddce
ayush-volmex <ayush@volmexlabs.com>
2021-12-22 12:07:19 +0530

    VOL-509: updated methods in interface

--- contracts/interfaces/IVolmexOracle.sol
+++ contracts/interfaces/IVolmexOracle.sol
@@ -3,12 +3,24 @@
 pragma solidity =0.8.11;
 
 interface IVolmexOracle {
-
-    function getVolatilityTokenPriceByIndex(uint256 _index) external view returns (uint256, uint256);
+    function getVolatilityTokenPriceByIndex(uint256 _index)
+        external
+        view
+        returns (uint256, uint256);
 
     function updateVolatilityTokenPrice(
-        string calldata _volatilityTokenSymbol,
-        uint256 _volatilityTokenPrice
+        uint256 _volatilityIndex,
+        uint256 _volatilityTokenPrice,
+        uint256 _index,
+        bytes32 _proofHash
     ) external;
 
+    function updateVolatilityCapRatio(uint256 _index, uint256 _volatilityCapRatio) external;
+
+    function addVolatilityTokenPrice(
+        uint256 _volatilityTokenPrice,
+        uint256 _index,
+        string calldata _volatilityTokenSymbol,
+        bytes32 _proofHash
+    ) external;
 }
