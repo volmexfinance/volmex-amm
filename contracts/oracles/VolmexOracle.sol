ea5dda7b15f1be1f055d3c2183990ba39160f612
ayush-volmex <ayush@volmexlabs.com>
2021-12-22 10:05:28 +0530

    VOL-509: added methods for leverage change

--- contracts/oracles/VolmexOracle.sol
+++ contracts/oracles/VolmexOracle.sol
@@ -21,8 +21,12 @@ contract VolmexOracle is OwnableUpgradeable {
         uint256 volatilityTokenPrice
     );
 
+    event VolatilityCapRatioUpdated(uint256 indexed _index, uint256 _volatilityCapRatio);
+
     // Store the price of volatility by indexes { 0 - ETHV, 1 = BTCV }
-    mapping(uint256 => uint256) public volatilityTokenPriceByIndex;
+    mapping(uint256 => uint256) private _volatilityTokenPriceByIndex;
+    // Store the volatilitycapratio by index
+    mapping(uint256 => uint256) public volatilityCapRatioByIndex;
     // Store the proof of hash of the current volatility token price
     mapping(uint256 => bytes32) public volatilityTokenPriceProofHash;
     // Store the symbol of volatility per index
@@ -33,10 +37,10 @@ contract VolmexOracle is OwnableUpgradeable {
     /**
      * @notice Used to check the volatility token price
      */
-    modifier _checkVolatilityPrice(uint256 _volatilityTokenPrice) {
+    modifier _checkVolatilityPrice(uint256 _index, uint256 _volatilityTokenPrice) {
         require(
-            _volatilityTokenPrice <= 250000000,
-            'VolmexOracle: _volatilityTokenPrice should be in range of 0 to 250'
+            _volatilityTokenPrice <= volatilityCapRatioByIndex[_index],
+            'VolmexOracle: _volatilityTokenPrice should be greater than 1000000'
         );
         _;
     }
@@ -46,13 +50,14 @@ contract VolmexOracle is OwnableUpgradeable {
      */
     function initialize() external initializer {
         __Ownable_init();
-        volatilityTokenPriceByIndex[indexCount] = 125000000;
+        _volatilityTokenPriceByIndex[indexCount] = 125000000;
         volatilityTokenPriceProofHash[indexCount] = ''; // Add proof of hash bytes32 value
         volatilityIndexBySymbol['ETHV'] = indexCount;
+        volatilityCapRatioByIndex[indexCount] = 250000000;
 
         indexCount++;
 
-        volatilityTokenPriceByIndex[indexCount] = 125000000;
+        _volatilityTokenPriceByIndex[indexCount] = 125000000;
         volatilityTokenPriceProofHash[indexCount] = ''; // Add proof of hash bytes32 value
         volatilityIndexBySymbol['BTCV'] = indexCount;
     }
@@ -67,6 +72,7 @@ contract VolmexOracle is OwnableUpgradeable {
      *
      * @param _volatilityIndex Number value of the volatility index. { eg. 0 }
      * @param _volatilityTokenPrice Price of volatility token, between {0, 250000000}
+     * @param _index index of volatilitycap ratio
      * @param _proofHash Bytes32 value of token price proof of hash
      *
      * NOTE: Make sure the volatility token price are with 6 decimals, eg. 125000000
@@ -74,40 +80,57 @@ contract VolmexOracle is OwnableUpgradeable {
     function updateVolatilityTokenPrice(
         uint256 _volatilityIndex,
         uint256 _volatilityTokenPrice,
+        uint256 _index,
         bytes32 _proofHash
-    )
-        external
-        onlyOwner
-        _checkVolatilityPrice(_volatilityTokenPrice)
-    {
-        volatilityTokenPriceByIndex[_volatilityIndex] = _volatilityTokenPrice;
+    ) external onlyOwner _checkVolatilityPrice(_index, _volatilityTokenPrice) {
+        _volatilityTokenPriceByIndex[_volatilityIndex] = _volatilityTokenPrice;
         volatilityTokenPriceProofHash[_volatilityIndex] = _proofHash;
 
         emit VolatilityTokenPriceUpdated(_volatilityTokenPrice, _volatilityIndex, _proofHash);
     }
 
     /**
+     * @notice Updates the volatility cap ratio by index
+     *
+     * @dev Check if volatility token price is greater than zero (0)
+     * @dev Update the volatility token price corresponding to the volatility token symbol
+     * @dev Store the volatility token price corresponding to the block number
+     * @dev Update the proof of hash for the volatility token price
+     *
+     * @param _index Number value of the volatility index. { eg. 0 }
+     * @param _volatilityCapRatio volatility cap ratio, between {0, 250000000}
+     *
+     * NOTE: Make sure the volatility cap ratio are with 6 decimals, eg. 125000000
+     */
+    function updateVolatilityCapRatio(uint256 _index, uint256 _volatilityCapRatio) external onlyOwner {
+        require(
+            _volatilityCapRatio >= 1000000,
+            'VolmexOracle: volatility cap ratio should be greater than 1000000'
+        );
+        volatilityCapRatioByIndex[_index] = _volatilityCapRatio;
+
+        emit VolatilityCapRatioUpdated(_index, _volatilityCapRatio);
+    }
+
+    /**
      * @notice Add volatility token price by index
      *
      * @param _volatilityTokenPrice Price of the adding volatility token
+     * @param _index index of volatilitycap ratio
      * @param _volatilityTokenSymbol Symbol of the adding volatility token
      * @param _proofHash Bytes32 value of token price proof of hash
      */
     function addVolatilityTokenPrice(
         uint256 _volatilityTokenPrice,
+        uint256 _index,
         string calldata _volatilityTokenSymbol,
         bytes32 _proofHash
-    ) external onlyOwner _checkVolatilityPrice(_volatilityTokenPrice) {
-        volatilityTokenPriceByIndex[++indexCount] = _volatilityTokenPrice;
+    ) external onlyOwner _checkVolatilityPrice(_index, _volatilityTokenPrice) {
+        _volatilityTokenPriceByIndex[++indexCount] = _volatilityTokenPrice;
         volatilityTokenPriceProofHash[indexCount] = _proofHash;
         volatilityIndexBySymbol[_volatilityTokenSymbol] = indexCount;
 
-
-        emit VolatilityTokenPriceAdded(
-            indexCount,
-            _volatilityTokenSymbol,
-            _volatilityTokenPrice
-        );
+        emit VolatilityTokenPriceAdded(indexCount, _volatilityTokenSymbol, _volatilityTokenPrice);
     }
 
     /**
@@ -115,9 +138,27 @@ contract VolmexOracle is OwnableUpgradeable {
      *
      * @param _volatilityTokenSymbol Symbol of the volatility token
      */
-    function getVolatilityPriceBySymbol(
-        string calldata _volatilityTokenSymbol
-    ) external view returns (uint256 volatilityTokenPrice) {
-        volatilityTokenPrice = volatilityTokenPriceByIndex[volatilityIndexBySymbol[_volatilityTokenSymbol]];
+    function getVolatilityPriceBySymbol(string calldata _volatilityTokenSymbol)
+        external
+        view
+        returns (uint256 volatilityTokenPrice)
+    {
+        volatilityTokenPrice = _volatilityTokenPriceByIndex[
+            volatilityIndexBySymbol[_volatilityTokenSymbol]
+        ];
+    }
+
+    /**
+     * @notice Get the volatility token price by index
+     *
+     * @param _index index of the volatility token
+     */
+    function getVolatilityTokenPriceByIndex(uint256 _index)
+        external
+        view
+        returns (uint256 volatilityTokenPrice, uint256 iVolatilityTokenPrice)
+    {
+        volatilityTokenPrice = _volatilityTokenPriceByIndex[_index];
+        iVolatilityTokenPrice = volatilityCapRatioByIndex[_index] - volatilityTokenPrice;
     }
 }
