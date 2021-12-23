e2eb5addb9ffbbfabda3ac0b491e7e4607e12b68
ayush-volmex <ayush@volmexlabs.com>
2021-12-23 11:33:18 +0530

    VOL-509: updated test case

--- test/VolmexOracle.test.ts
+++ test/VolmexOracle.test.ts
@@ -1,20 +1,22 @@
-const { expect } = require("chai");
-const { ethers, upgrades } = require("hardhat");
-import { Signer } from "ethers";
-const { expectRevert } = require("@openzeppelin/test-helpers");
+const { expect } = require('chai');
+const { ethers, upgrades } = require('hardhat');
+const assert = require('assert');
+import { Signer } from 'ethers';
+const { expectRevert } = require('@openzeppelin/test-helpers');
 
-
-describe("Volmex Oracle", function () {
+describe('Volmex Oracle', function () {
   let accounts: Signer[];
   let volmexOracleFactory: any;
   let volmexOracle: any;
+  let volatilityIndexes: any;
+  let volatilityTokenPrices: any;
+  let indexes: any;
+  let proofHashes: any;
 
   this.beforeAll(async function () {
     accounts = await ethers.getSigners();
 
-    volmexOracleFactory = (await ethers.getContractFactory(
-      "VolmexOracle"
-    ));
+    volmexOracleFactory = await ethers.getContractFactory('VolmexOracle');
   });
 
   this.beforeEach(async function () {
@@ -23,40 +25,85 @@ describe("Volmex Oracle", function () {
     await volmexOracle.deployed();
   });
 
-  it ("Should deplloy volmex oracle", async () => {
+  it.only('Should deploy volmex oracle', async () => {
     const receipt = await volmexOracle.deployed();
     expect(receipt.confirmations).not.equal(0);
   });
 
-  it ("Should update the volatility price", async () => {
-    const receipt = await volmexOracle.updateVolatilityTokenPrice(
-      "0",
-      "105",
-      "0x6c00000000000000000000000000000000000000000000000000000000000000"
+  it.only('Should update the volatility price', async () => {
+    volatilityIndexes = ['0'];
+    volatilityTokenPrices = ['105000000'];
+    indexes = ['0'];
+    proofHashes = ['0x6c00000000000000000000000000000000000000000000000000000000000000'];
+    const receipt = await volmexOracle.updateBatchVolatilityTokenPrice(
+      volatilityIndexes,
+      volatilityTokenPrices,
+      indexes,
+      proofHashes
     );
-
     expect((await receipt.wait()).confirmations).not.equal(0);
-
-    expect(await volmexOracle.volatilityTokenPriceByIndex("0")).equal("1050000");
+    let prices = await volmexOracle.getVolatilityTokenPriceByIndex('0');
+    assert.equal(prices[0].toString(), '105000000');
   });
 
-  it ("Should revert when volatility price is not in range", async () => {
+  it.only('Should not update if volatility price greater than cap ratio', async () => {
+    volatilityIndexes = ['0'];
+    volatilityTokenPrices = ['2105000000'];
+    indexes = ['0'];
+    proofHashes = ['0x6c00000000000000000000000000000000000000000000000000000000000000'];
     await expectRevert(
-      volmexOracle.updateVolatilityTokenPrice(
-        "0",
-        "250",
-        "0x6c00000000000000000000000000000000000000000000000000000000000000"
+      volmexOracle.updateBatchVolatilityTokenPrice(
+        volatilityIndexes,
+        volatilityTokenPrices,
+        indexes,
+        proofHashes
       ),
-      "VolmexOracle: _volatilityTokenPrice should be greater than 0"
+      'VolmexOracle: _volatilityTokenPrice should be smaller than VolatilityCapRatio'
     );
+  });
 
+  it.only('should revert when length of input arrays are not equal', async () => {
     await expectRevert(
-      volmexOracle.updateVolatilityTokenPrice(
-        "1",
-        "0",
-        "0x6c00000000000000000000000000000000000000000000000000000000000000"
+      volmexOracle.updateBatchVolatilityTokenPrice(
+        ['0', '1'],
+        ['105000000'],
+        ['0'],
+        ['0x6c00000000000000000000000000000000000000000000000000000000000000']
       ),
-      "VolmexOracle: _volatilityTokenPrice should be greater than 0"
+      'VolmexOracle: length of arrays input are not equal'
+    );
+  });
+
+  it.only('should add volatility index', async () => {
+    await volmexOracle.addVolatilityIndex(
+      '105000000',
+      '250000000',
+      'ETHV3x',
+      '0x6c00000000000000000000000000000000000000000000000000000000000000'
+    );
+    const index = await volmexOracle.volatilityIndexBySymbol('ETHV3x');
+    const price = await volmexOracle.getVolatilityPriceBySymbol('ETHV3x');
+    await volmexOracle.addVolatilityIndex(
+      '100000000',
+      '350000000',
+      'ETHV4x',
+      '0x6c00000000000000000000000000000000000000000000000000000000000000'
+    );
+    const price1 = await volmexOracle.getVolatilityPriceBySymbol('ETHV4x');
+    assert.equal(price1[0].toString(), '100000000');
+    assert.equal(price[0].toString(), '105000000');
+    assert.equal(index, 2);
+  });
+
+  it.only('should revert when cap ratio is smaller than 1000000', async () => {
+    await expectRevert(
+      volmexOracle.addVolatilityIndex(
+        '125000000',
+        '250',
+        'ETHV2x',
+        '0x6c00000000000000000000000000000000000000000000000000000000000000'
+      ),
+      'VolmexOracle: volatility cap ratio should be greater than 1000000'
     );
   });
 });
