96015e7638cc43e560b191ddd70a20148eaf0469
vijay-volmex <wankhedevijay04@gmail.com>
2021-12-28 14:43:34 +0530

    VOL-520: Inherited oz IERC20

--- contracts/interfaces/IERC20Modified.sol
+++ contracts/interfaces/IERC20Modified.sol
@@ -2,23 +2,12 @@
 
 pragma solidity =0.8.11;
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
 /**
  * @dev Modified Interface of the OpenZeppelin's IERC20 extra functions to add features in position token.
  */
-interface IERC20Modified {
-    // IERC20 Methods
-    function totalSupply() external view returns (uint256);
-
-    function balanceOf(address account) external view returns (uint256);
-
-    function transfer(address recipient, uint256 amount) external returns (bool);
-
-    function allowance(address owner, address spender) external view returns (uint256);
-
-    function approve(address spender, uint256 amount) external returns (bool);
-
-    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
-
+interface IERC20Modified is IERC20 {
     // Custom Methods
     function symbol() external view returns (string memory);
 
