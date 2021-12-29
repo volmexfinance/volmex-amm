a726a01f70d2cdba6c07d245c5e8ae9a712beb81
ayush-volmex <ayush@volmexlabs.com>
2021-12-29 17:09:55 +0530

    VOL_507: removed unnecessary imports

--- contracts/repricers/VolmexRepricer.sol
+++ contracts/repricers/VolmexRepricer.sol
@@ -2,7 +2,6 @@
 
 pragma solidity =0.8.11;
 
-import '@openzeppelin/contracts-upgradeable/utils/AddressUpgradeable.sol';
 import '@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20PausableUpgradeable.sol';
 import '@openzeppelin/contracts-upgradeable/utils/introspection/ERC165StorageUpgradeable.sol';
 
