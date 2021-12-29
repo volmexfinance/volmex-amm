bf66f3f75ff7f21182f253471a43056ca082c70d
vijay-volmex <wankhedevijay04@gmail.com>
2021-12-29 18:30:49 +0530

    VOL-520: Revert the optimizer runs to 2000

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -38,7 +38,7 @@ const config: HardhatUserConfig = {
         settings: {
           optimizer: {
             enabled: true,
-            runs: 200,
+            runs: 2000,
           },
         },
       },
@@ -47,7 +47,7 @@ const config: HardhatUserConfig = {
         settings: {
           optimizer: {
             enabled: true,
-            runs: 200,
+            runs: 2000,
           },
         },
       },
