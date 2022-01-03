06d51dee085f19958a868f858edeafcd82064bf1
vijay-volmex <wankhedevijay04@gmail.com>
2022-01-03 19:05:31 +0530

    VOL-520: Updated the code for test collateral helper

--- contracts/protocol/TestCollateralToken.sol
+++ contracts/protocol/TestCollateralToken.sol
@@ -5,7 +5,17 @@ pragma solidity =0.8.11;
 import "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetMinterPauser.sol";
 
 contract TestCollateralToken is ERC20PresetMinterPauser {
-    constructor(string memory symbol) ERC20PresetMinterPauser("VolmexTestCollateralToken", symbol) {
-        mint(msg.sender, 100000000000000000000000000000000);
+    uint8 private decimal;
+    constructor(
+        string memory _symbol,
+        uint256 _initSupply,
+        uint8 _decimal
+    ) ERC20PresetMinterPauser("VolmexTestCollateralToken", _symbol) {
+        mint(msg.sender, _initSupply);
+        decimal = _decimal;
+    }
+
+    function decimals() public view override returns (uint8) {
+        return decimal;
     }
 }
