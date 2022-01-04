ff949e2f0700a48192ab52623cb8bae121a2e262
ayush-volmex <ayush@volmexlabs.com>
2022-01-04 19:23:08 +0530

    VOL-520: Moved events above

--- contracts/interfaces/IEIP20NonStandard.sol
+++ contracts/interfaces/IEIP20NonStandard.sol
@@ -8,39 +8,36 @@ pragma solidity =0.8.11;
  * See https://medium.com/coinmonks/missing-return-value-bug-at-least-130-tokens-affected-d67bf08521ca
  */
 interface IEIP20NonStandard {
+    event Transfer(address indexed from, address indexed to, uint256 amount);
+    event Approval(address indexed owner, address indexed spender, uint256 amount);
     /**
      * @notice Get the total number of tokens in circulation
      * @return The supply of tokens
      */
     function totalSupply() external view returns (uint256);
-
     /**
      * @notice Gets the balance of the specified address
      * @param owner The address from which the balance will be retrieved
      * @return balance The balance
      */
     function balanceOf(address owner) external view returns (uint256 balance);
-
     /** 
     *
     * !!!!!!!!!!!!!!
     * !!! NOTICE !!! `transfer` does not return a value, in violation of the ERC-20 specification
     * !!!!!!!!!!!!!!
     *
-
     * @notice Transfer `amount` tokens from `msg.sender` to `dst`
     * @param dst The address of the destination account
     * @param amount The number of tokens to transfer
     */
     function transfer(address dst, uint256 amount) external;
-
     /** 
     *
     * !!!!!!!!!!!!!!
     * !!! NOTICE !!! `transferFrom` does not return a value, in violation of the ERC-20 specification
     * !!!!!!!!!!!!!!
     *
-
     * @notice Transfer `amount` tokens from `src` to `dst`
     * @param src The address of the source account
     * @param dst The address of the destination account
@@ -51,7 +48,6 @@ interface IEIP20NonStandard {
         address dst,
         uint256 amount
     ) external;
-
     /**
      * @notice Approve `spender` to transfer up to `amount` from `src`
      * @dev This will overwrite the approval amount for `spender`
@@ -61,7 +57,6 @@ interface IEIP20NonStandard {
      * @return success Whether or not the approval succeeded
      */
     function approve(address spender, uint256 amount) external returns (bool success);
-
     /**
      * @notice Get the current allowance from `owner` for `spender`
      * @param owner The address of the account which owns the tokens to be spent
@@ -69,7 +64,4 @@ interface IEIP20NonStandard {
      * @return remaining The number of tokens allowed to be spent
      */
     function allowance(address owner, address spender) external view returns (uint256 remaining);
-
-    event Transfer(address indexed from, address indexed to, uint256 amount);
-    event Approval(address indexed owner, address indexed spender, uint256 amount);
 }
