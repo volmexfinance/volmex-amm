74f928f0b6a655163c3244b8073f84f3d0ea7b7f
vijay-volmex <wankhedevijay04@gmail.com>
2022-02-10 16:34:18 +0530

    VOL-632: Added constant for 10 power 12

--- contracts/maths/Const.sol
+++ contracts/maths/Const.sol
@@ -4,8 +4,7 @@ pragma solidity =0.8.11;
 
 contract Const {
     uint256 public constant BONE = 10**18;
-
     int256 public constant iBONE = int256(BONE);
-
     uint256 public constant MAX_IN_RATIO = BONE / 2;
+    uint256 public constant POWER_12 = 10**12;
 }
