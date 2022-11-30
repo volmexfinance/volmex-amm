6d1d83a1be29eb116941028d97602eeb30bc0713
ayush-volmex <ayush@volmexlabs.com>
2022-11-30 16:54:22 +0530

    VOL-1301: Added migration contract for state stacks

---
+++ contracts/protocol/v1/migration/Migration.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: BUSL-1.1
+
+pragma solidity =0.8.11;
+
+import "../../../interfaces/IVolmexProtocol.sol";
+
+/**
+ * @title Migration contract
+ * @author volmex.finance [security@volmexlabs.com]
+ *
+ * This protocol is used to migrate V1 tokens to V2
+ */
+contract Migration {
+    // Address of V2 protocol
+    IVolmexProtocol public v2Protocol;
+}
