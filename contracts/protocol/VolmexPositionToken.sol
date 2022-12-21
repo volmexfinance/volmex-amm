8dac07bacea9c2ee85e48bc5598ec905a131aad2
ayush-volmex <ayush@volmexlabs.com>
2022-12-21 19:42:22 +0530

    VOL-1344: Added feature for bridging tokens to various chains

--- contracts/protocol/VolmexPositionToken.sol
+++ contracts/protocol/VolmexPositionToken.sol
@@ -1,11 +1,9 @@
 // SPDX-License-Identifier: BUSL-1.1
-
 pragma solidity =0.8.11;
-
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20PausableUpgradeable.sol";
-
+import "@layerzerolabs/contracts/lzApp/NonblockingApp.sol";
 /**
  * @title Token Contract
  * @author volmex.finance [security@volmexlabs.com]
@@ -13,14 +11,14 @@ import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20Pausable
 contract VolmexPositionToken is
     Initializable,
     AccessControlUpgradeable,
-    ERC20PausableUpgradeable
+    ERC20PausableUpgradeable,
+    NonblockingLzApp,
 {
+    
     event UpdatedTokenMetadata(string name, string symbol);
-
     // Position token role, calculated as keccak256("VOLMEX_PROTOCOL_ROLE")
     bytes32 public constant VOLMEX_PROTOCOL_ROLE =
         0x33ba6006595f7ad5c59211bde33456cab351f47602fc04f644c8690bc73c4e16;
-
     // Openzepplin's ERC20 name and symbol variables are private
     // To add functionality to update the token metadata, we added another variables
     // We added the override public-view method, to compensate on ERC20's methods
@@ -28,27 +26,28 @@ contract VolmexPositionToken is
     string private _vivName;
     // symbol of the token
     string private _vivSymbol;
-
+    // source id for chain
+    uint256 srcChainId ;
+    mapping(address => uint256) public lockedTokens;
     /**
      * @dev Grants `DEFAULT_ADMIN_ROLE` and `VOLMEX_PROTOCOL_ROLE` to the
      * account that deploys the contract.
      *
      * See {ERC20-constructor}.
      */
-    function initialize(string memory _name, string memory _symbol)
+    function initialize(string memory _name, string memory _symbol, address _lzEndPoint, uint256 _srcChainId)
         external
         initializer
+                NonblockingLzApp(_lzEndPoint)
     {
         __AccessControl_init_unchained();
-
         __ERC20Pausable_init();
+        srcChainId = _srcChainId;
         _vivName = _name;
-        _vivSymbol = _symbol;
-
+        _vivSymbol = _symbol; 
         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _setupRole(VOLMEX_PROTOCOL_ROLE, msg.sender);
     }
-
     /**
      * @dev Updates token name & symbol of VIV tokens
      *
@@ -64,8 +63,39 @@ contract VolmexPositionToken is
         _vivSymbol = _symbol;
         emit UpdatedTokenMetadata(_name, _symbol);
     }
-
-
+    // TODO wrapper token mint functionality for Polygon chain
+    // to transfer tokens from one chain to other
+    function crossChainTransfer(uint16 dstChainId,address token, address _form, uint amount ) external payable {
+              require(allowance(from, address(this))>=amount, "Insuffiecient Allowance" );
+                require(msg.sender != address(0), "Caller cannot be address 0");
+                require(_from)
+        if(srcChainId == 10102){
+          transferFrom(_from,address(this), amount);
+                    lockedToken[_from]+= amount;
+          bytes memory payload = abi.encode(_from, amount);
+         _lzSend(dstChainId, payload, payable(_from), address(0x0), bytes(''));
+        }else if(srcChainid = 10109){
+        // interface(token).burn(from,amount)
+                // _lzSend(dstChainId, payload, payable(_from), address(0x0), bytes(''));
+                } else {
+         burn(_from, amount);
+         bytes memory payload = abi.encode(_from, amount);
+         _lzSend(dstChainId, payload, payable(_from), address(0x0), bytes(''));
+        }
+    }
+    // to receive tokens from another
+        function _nonblockingLzReceive(uint16 _srcChainId, bytes memory, address srcAddress, bytes memory _payload) internal override{
+            (address _from , uint256 amount) = abi.decode(_payload, (address,uint));
+            if(srcChainId == 10109){
+                // interface(token).burn(from,amount); 
+                // _lzSend(dstChainId, payload, payable(_from), address(0x0), bytes(''));
+            }else if(srcChainId = 10102) {
+       lockedToken[_from] -= amount;
+             transfer(_from , amount);
+            }else {
+       mint(_from, amount);
+            }
+        }
     /**
      * @dev Creates `amount` new tokens for `to`.
      *
@@ -82,7 +112,6 @@ contract VolmexPositionToken is
         );
         _mint(_to, _amount);
     }
-
     /**
      * @dev Destroys `amount` tokens from the caller.
      *
@@ -95,7 +124,6 @@ contract VolmexPositionToken is
         );
         _burn(_from, _amount);
     }
-
     /**
      * @dev Pauses all token transfers.
      *
@@ -112,7 +140,6 @@ contract VolmexPositionToken is
         );
         _pause();
     }
-
     /**
      * @dev Unpauses all token transfers.
      *
@@ -129,14 +156,12 @@ contract VolmexPositionToken is
         );
         _unpause();
     }
-
     /**
      * @dev Returns the name of the token.
      */
     function name() public view virtual override returns (string memory) {
         return _vivName;
     }
-
     /**
      * @dev Returns the symbol of the token, usually a shorter version of the
      * name.
